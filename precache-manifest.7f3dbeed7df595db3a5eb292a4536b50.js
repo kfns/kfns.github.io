ad0690af2cfd1615ec5a4dd1c5f933769d3c0275
chess123 <kesav@viswanadha.net>
2020-03-22 20:52:16 -0700

    Updates

---
+++ precache-manifest.7f3dbeed7df595db3a5eb292a4536b50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1dc68b2bfc78a22e8e0c4d0e9ac54b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67cb321dee563caf1b31",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f74e160c65ab1786eb5",
+    "url": "/static/js/2.3251d721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3251d721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cb321dee563caf1b31",
+    "url": "/static/js/main.b2ec82e9.chunk.js"
+  },
+  {
+    "revision": "66f87f07be7ae4aeb0ff",
+    "url": "/static/js/runtime-main.8f22c9ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
