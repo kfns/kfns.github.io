c9b1c8603cdf472de8383060220fedb07d383065
chess123 <kesav@viswanadha.net>
2020-03-24 18:16:00 -0700

    Revert "Add CNAME"
    This reverts commit 7cda9a1f08016a3e26fe4b00d6f8b1fbd7aa6ce1.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Template from './Template';
-import Home from './Home';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/test" component={Template} />
-        </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
